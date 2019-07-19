6402029f45350196f25752713b7e1fecbac323bf
shammyasaha1989 <shammya.saha@asu.edu>
2019-07-18 17:25:12 -0700

    Comments added

--- MATLAB/getYPrim.m
+++ MATLAB/getYPrim.m
@@ -22,7 +22,6 @@ setSourceInfo(DSSObj,{'source'},'pu',Baseparam.slack_voltage);
 % minimum voltage at each time step this has to set as 1
 DSSSolution.Number= 1; 
 % Variable to save the minimum per unit of all the buses
-v= zeros(1,8760);
 yy=zeros(8760,18);
 %%
 power_flow = tic;
@@ -31,31 +30,18 @@ for i = 1:8760
     if (~DSSSolution.Converged)
         error('Solution Not Converged. Check Model for Convergence');
     end
-% Use of the library puts 0 to absent phases to the buses, hence i had to
-% filter out the buses with 0 pu voltage
-%     bus = getBusInfo(DSSObj);
-%     voltage = [bus.phaseVoltagesPU];
-%     zero_points = find(voltage ==0);
-%     voltage (zero_points) = [];
-%     v (1,i)= min(voltage);
     DSSObj.ActiveCircuit.SetActiveElement('Load.671');
-    yy(i,:) = DSSObj.ActiveCircuit.ActiveCktElement.Yprim;
+    yy(i,:) = DSSObj.ActiveCircuit.ActiveCktElement.Yprim; % Get and save the Y prim of load 671, the variable is saved as a MAT file, check the odd column 
+%     entries for each row as that correspond to the real power portion of the load.
     
 end
 t = toc (power_flow);
 total_time = toc (start_time);
-plot(v) % plotting the minimum voltage 
-
-sprintf('Displaying Power Flow Solution Time: %f', t) % The output value is 710.418 
-sprintf('Displaying Total Time: %f', total_time)% The output value is 712.945
 
 
-% %%
-% DSSText.command = 'Compile C:\feeders\MultiPhase\13BusQSTS\IEEE13Nodeckt.dss';
-% setSourceInfo(DSSObj,{'source'},'pu',Baseparam.slack_voltage);
-% qsts_start = tic;
-% DSSSolution.Number= 8760; 
-% DSSSolution.Solve(); % because the solution number is set as 8760, we no longer can retrieve values at each time step using the COM interface, and hence calcualting the minimum voltage at each time is not possible
-% t = toc(qsts_start);
-% sprintf('Displaying Yearly Solution Time: %f', t)
+sprintf('Displaying Power Flow Solution Time: %f', t) 
+sprintf('Displaying Total Time: %f', total_time)
+
+
+
 
